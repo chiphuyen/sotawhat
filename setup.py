babc43f7bf6b491691d47625c78b35bc06d6a3dd
Chip Huyen <huyenchip@alumni.stanford.edu>
2018-11-09 00:02:45 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,17 +6,12 @@ setup(
     version=str(sotawhat.__VERSION__),
     packages=find_packages(),
     description='arxiv-sanity query script',
-    long_description=str('I often get frustrated searching for the latest '
-                         'research results on Google and Arxiv so I wrote '
-                         'SOTAwhat, a script to query Arxiv for the latest '
+    long_description=str('SOTAwhat is a script to query Arxiv for the latest '
                          'abstracts and extract summaries from them. '),
     url='https://huyenchip.com/2018/10/04/sotawhat.html',
     license="",
-    install_requires=['six', 'nltk'],
-    extras_require={
-        'full': ['PyEnchant'],
-    },
+    install_requires=['six', 'nltk', 'pyspellchecker'],
     entry_points={
-        'console_scripts': ['sotawhat=sotawhat.cmd_line:main'],
+        'console_scripts': ['sotawhat=sotawhat.sotawhat:main'],
     }
 )
